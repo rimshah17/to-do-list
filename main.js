fa26804c0b0c0304563866d4ea332d710d0c2bc2
rimshah17 <rimshahali11@gmail.com>
2024-06-02 04:34:01 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let todoQuestion = await inquirer.prompt([
+        {
+            name: "firstQuestion",
+            type: "input",
+            message: "what would you like to add in your todos?"
+        },
+        {
+            name: "secondQuestion",
+            type: "confirm",
+            message: " whould you like to add more in your todos?"
+        }
+    ]);
+    todos.push(todoQuestion.firstQuestion);
+    console.log(todos);
+    condition = todoQuestion.secondQuestion;
+}
